d93a1f41735896c7fed4f37841f3fb2a5de5e765
luckytyphlosion <alan.rj.huang@gmail.com>
2015-05-21 18:19:55 -0400

    Create conditional_evos_moves.asm

---
+++ engine/conditional_evos_moves.asm
@@ -0,0 +1,23 @@
+_EvSylveon::
+  ld bc, wPartyMon2 - wPartyMon1
+  ld hl, wPartyMon1Moves
+
+_EvPartyMon::
+
+_EvHeldItem::
+
+_EvPartyMonType::
+
+_EvSurfing::
+
+_EvGender::
+
+_EvStatBased::
+
+_EvMove::
+
+_EvFriendship::
+
+_EvLocation::
+
+_EvShedinja::
