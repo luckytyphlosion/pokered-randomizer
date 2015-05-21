1c747b731219316bc3fd1f99da5173992ce19b9e
luckytyphlosion <alan.rj.huang@gmail.com>
2015-05-21 16:36:53 -0400

    New evolution constants.

--- constants/evolution_constants.asm
+++ constants/evolution_constants.asm
@@ -2,3 +2,20 @@
 EV_LEVEL EQU 1
 EV_ITEM  EQU 2
 EV_TRADE EQU 3
+
+; Evolution Sub-Types
+; EV_NORMAL EQU 0 ; normal level up
+EV_SYLVEON EQU 1 ; have fairy type move
+EV_PARTYMON EQU 2 ; have certain pokemon in party. Currently only used for mantyke -> mantine
+EV_HELDITEM EQU 3
+EV_PARTYMONTYPE EQU 4 ; pancham -> pangoro
+EV_SURFING EQU 5 ; sliggoo -> goodra
+EV_GENDER EQU 6 ; female combee -> vespiquen | male burmy -> mothim | female burmy -> wormadam
+; also used for wurmple
+EV_STATBASED EQU 7 ; tyrogue
+EV_MOVE EQU 8
+EV_FRIENDSHIP EQU 9 ; stat-exp based
+EV_LOCATION EQU 10 ; location based
+
+; Evo Sub-Sub-Types
+EV_TIME EQU 1
